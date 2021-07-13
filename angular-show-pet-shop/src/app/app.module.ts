ed018769821472f7625b7733191dbda789120f0d
cuongpham270695 <cuong.pham270695@gmail.com>
2021-07-13 13:33:36 +0700

    angular_show_pet_info

---
+++ angular-show-pet-shop/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PetComponent } from './components/pet/pet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PetComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
